35c05e6c8eb057c247981588f23391919c89a776
Sai-56 <bsaishreyas@gmail.com>
2020-12-30 14:57:50 -0600

    Converted to classes, added PCA.

--- term-term.py
+++ term-term.py
@@ -1,8 +1,92 @@
+import pickle
+import numpy as np
 import operator
 from itertools import islice
 from itertools import tee
 
 import time
+
+
+class Embedding:
+
+    def __init__(self, vocab):
+        self.vocab = vocab
+        self.dictn = {}
+
+    def getCos(self,  word1, word2):
+        targetWord1 = []
+        targetWord2 = []
+
+        for item in self.dictn.items():
+            if item[0][0] == word1:
+                targetWord1.append(item)
+            if item[0][0] == word2:
+                targetWord2.append(item)
+
+        commonWords1 = []
+        commonWords2 = []
+
+        for item in targetWord1:
+            target = item[0][1]
+            for item2 in targetWord2:
+                if(item2[0][1] == target):
+                    commonWords1.append(item)
+        for item in targetWord2:
+            target = item[0][1]
+            for item2 in targetWord1:
+                if(item2[0][1] == target):
+                    commonWords2.append(item)
+
+        cw1 = {}
+        cw = []
+        for i in commonWords1:
+            cw1[i[0][1]] = i[1]
+            cw.append(i[0][1])
+        cw2 = {}
+        for i in commonWords2:
+            cw2[i[0][1]] = i[1]
+
+        num = 0
+        denw1 = 0
+        denw2 = 0
+
+        for i in range(len(cw1)):
+            num += cw1[cw[i]] * cw2[cw[i]]
+            denw1 += cw1[cw[i]]**2
+            denw2 += cw2[cw[i]]**2
+        try:
+            return num/((denw1**.5)*(denw2**.5))
+        except:
+            return("0.0")
+
+
+class TermTermEmbedding(Embedding):
+
+    def __init__(self, vocab,  window, iterable):
+        super().__init__(vocab)
+        context = []
+        q = 0
+        while q < window*2+1:
+            w = next(iterable)
+            context.append(w)
+            q = q+1
+
+        for target in iterable:
+            for j in range(len(context)):
+                if context[window] in self.vocab and context[j] in self.vocab and j != window:
+                    if (context[window], context[j]) in self.dictn:
+                        self.dictn[(context[window], context[j])] += 1
+                    else:
+                        self.dictn[(context[window], context[j])] = 1
+            context.pop(0)
+            context.append(target)
+
+        for j in range(len(context)):
+            if context[window] in self.vocab and context[j] in self.vocab and j != window:
+                if (context[window], context[j]) in self.dictn:
+                    self.dictn[(context[window], context[j])] += 1
+                else:
+                    self.dictn[(context[window], context[j])] = 1
 
 
 def getCos(d, word1, word2):
@@ -23,14 +107,29 @@ def getCos(d, word1, word2):
         for item2 in targetWord2:
             if(item2[0][1] == target):
                 commonWords1.append(item)
-                commonWords2.append(item2)
+    for item in targetWord2:
+        target = item[0][1]
+        for item2 in targetWord1:
+            if(item2[0][1] == target):
+                commonWords2.append(item)
+
+    cw1 = {}
+    cw = []
+    for i in commonWords1:
+        cw1[i[0][1]] = i[1]
+        cw.append(i[0][1])
+    cw2 = {}
+    for i in commonWords2:
+        cw2[i[0][1]] = i[1]
+
     num = 0
     denw1 = 0
     denw2 = 0
-    for l, n in zip(commonWords1, commonWords2):
-        num += l[1]*n[1]
-        denw1 += l[1]**2
-        denw2 += n[1]**2
+
+    for i in range(len(cw1)):
+        num += cw1[cw[i]] * cw2[cw[i]]
+        denw1 += cw1[cw[i]]**2
+        denw2 += cw2[cw[i]]**2
     try:
         return num/((denw1**.5)*(denw2**.5))
     except:
@@ -40,50 +139,6 @@ def getCos(d, word1, word2):
 def take(n, iterable):
     return list(islice(iterable, n))
 
-
-def getMatrix(arr, window=4):
-    # it = iter(arr)
-    vocab = {}
-    gen1, gen2 = tee(arr)
-
-    for i in gen1:
-        if i in vocab.keys():
-            vocab[i] += 1
-        else:
-            vocab[i] = 1
-
-    # it = iter(arr)
-    vocab = dict(
-        sorted(vocab.items(), key=operator.itemgetter(1), reverse=True))
-    vocab = dict(take(1000, vocab.items()))
-
-    dictn = {}
-    context = []
-
-    q = 0
-    while q < window*2+1:
-        w = next(gen2)
-        context.append(w)
-        q = q+1
-
-    for target in gen2:
-        for j in range(len(context)):
-            if context[window] in vocab and context[j] in vocab and j != window:
-                if (context[window], context[j]) in dictn:
-                    dictn[(context[window], context[j])] += 1
-                else:
-                    dictn[(context[window], context[j])] = 1
-        context.pop(0)
-        context.append(target)
-
-    for j in range(len(context)):
-        if context[window] in vocab and context[j] in vocab and j != window:
-            if (context[window], context[j]) in dictn:
-                dictn[(context[window], context[j])] += 1
-            else:
-                dictn[(context[window], context[j])] = 1
-
-    return dictn
 
 
 def readFileWGenerator(file):
@@ -100,35 +155,123 @@ def readFileWGenerator(file):
     yield word
 
 
-def readFileWList(file):
-    file = open(file)
-    arr = file.readlines()
-    arr = arr[0].split(" ")
-    return arr
-
-# file = open("demo.txt")
 
 
-# def f():
-#     dat = file.read(1)
-#     word = ""
-#     while dat:
-#         if dat == " ":
-#             yield word
-#             word = ""
-#         else:
-#             word = word+dat
-#         dat = file.read(1)
-#     yield word
+def _PCA(X, num_components):
 
-# f = f()
-# for i in f:
-#     print(i)
+    # Step-1
+    X_meaned = X - np.mean(X, axis=0)
+
+    # Step-2
+    cov_mat = np.cov(X_meaned, rowvar=False)
+
+    # Step-3
+    variance_explained = []
+    eigen_values, eigen_vectors = np.linalg.eigh(cov_mat)
+    for i in eigen_values:
+        variance_explained.append((i/sum(eigen_values))*100)
+    # As you can see, the first two principal components contain 55% of
+    print("Variance:", np.cumsum(
+        [i*(100/sum(eigen_values)) for i in eigen_values]))
+
+    # Step-4
+    sorted_index = np.argsort(eigen_values)[::-1]
+    sorted_eigenvalue = eigen_values[sorted_index]
+    sorted_eigenvectors = eigen_vectors[:, sorted_index]
+
+    # Step-5
+    eigenvector_subset = sorted_eigenvectors[:, 0:num_components]
+
+    # Step-6
+    X_reduced = np.dot(eigenvector_subset.transpose(),
+                       X_meaned.transpose()).transpose()
+
+    return X_reduced
+
+# X = np.random.randint(10,50,100).reshape(20,5)
+
+
+def convertToMatrix(dictn):
+    keys = np.array(list(dictn.keys()))
+    vals = np.array(list(dictn.values()))
+
+    unq_keys, key_idx = np.unique(keys, return_inverse=True)
+
+    key_idx = key_idx.reshape((-1, 2))
+    n = len(unq_keys)
+    adj = np.zeros((n, n), dtype=vals.dtype)
+    adj[key_idx[:, 0], key_idx[:, 1]] = vals
+
+    a = adj[np.where(unq_keys == "food")].astype("int64")
+    b = adj[np.where(unq_keys == "one")].astype("int64")
+    # print(unq_keys)
+
+    num = 0
+    denw1 = 0
+    denw2 = 0
+
+    for i in range(len(a)):
+        num = num + a[i] * b[i]
+        denw1 += a[i] ** 2
+        denw2 += b[i] ** 2
+
+    numerator = np.sum(num)
+    den = np.sqrt(np.sum(denw1))
+    den1 = np.sqrt(np.sum(denw2))
+
+    print("Cos by Matrix:", numerator/(den*den1))
+    print("Cos by dict:", getCos(dictn, "food", "one"))
+
+    _adj = _PCA(adj, 1500)
+
+    a1 = _adj[np.where(unq_keys == "food")].astype("int64")
+    b1 = _adj[np.where(unq_keys == "one")].astype("int64")
+    num = 0
+    denw1 = 0
+    denw2 = 0
+
+    for i in range(len(a)):
+        num += a1[i] * b1[i]
+        denw1 += a1[i] ** 2
+        denw2 += b1[i] ** 2
+
+    numerator = np.sum(num)
+    den = np.sqrt(np.sum(denw1))
+    den1 = np.sqrt(np.sum(denw2))
+
+    print("Cos by PCA:", numerator/(den*den1))
+
+
+def getVocab(file, vocabSize):
+    """given a text file, returns a tuple of the vocabulary of the most frequent words and a generator"""
+
+    fileReader = readFileWGenerator(file)
+
+    gen1, gen2 = tee(fileReader)
+    vocab = {}
+
+    for i in gen1:
+        if i in vocab.keys():
+            vocab[i] += 1
+        else:
+            vocab[i] = 1
+
+    vocab = dict(
+        sorted(vocab.items(), key=operator.itemgetter(1), reverse=True))
+    vocab = dict(take(vocabSize, vocab.items()))
+
+    return (vocab, gen2)
+
 
 start_time = time.time()
-fileData = readFileWGenerator("text8")
+# vocab = getVocab("text8", 1500)
+# E = TermTermEmbedding(vocab[0], 4, vocab[1])
+# with open('filename.pickle', 'wb') as handle:
+#     pickle.dump(E.dictn, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-dictData = getMatrix(fileData, 4)
-
-print(getCos(dictData, "there", "is"))
+with open('filename.pickle', 'rb') as handle:
+    E = pickle.load(handle)
+# print(E)
+# print(E.getCos("there", "is"))
+convertToMatrix(E)
 print("--- %s seconds ---" % (time.time() - start_time))
